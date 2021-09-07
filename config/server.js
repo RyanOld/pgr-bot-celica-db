e4d1c699e692154b65d0d52a799bf5d83f9215df
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2021-09-07 12:52:14 +0700

    first commit after name change

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dcdb73b2190c21f3048fe34135d43fd3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3e1a7dd-d9fa-4945-88a1-b0771d92f2fc'
+};
